27be5dff58624c150c553bdad7074af716a99539
n-avontuur <55791419+n-avontuur@users.noreply.github.com>
2021-12-29 22:37:43 +0100

    NOTHING BIG

--- WatermeterV2.py
+++ WatermeterV2.py
@@ -14,22 +14,25 @@ OpenhabUrl = "http://192.168.0.227:8080"
 #Open meterstand.txt file en lees meterstand
 #Als meterstand.txt niet aanwezig is maakt script bestand aan en vult de meterstand
 fn = "/home/pi/Documents/Scripts/meterstand_water.txt"
-if os.path.exists(fn):
-    f = file(fn, "r+")
-    f = open(fn)
-    inhoud = f.readline()
-    a,b,c = inhoud.split()
-    Counter = int(c)
-else:
-    f = open(fn, "w")
-    f.write( 'meterstand = ' + repr(Counter))
-    f.close()
 
 #Board is pin nr, BMC is GPIO nr
 #Read output from water meter op pin 40
 GPIO.setmode(GPIO.BOARD)
 # Set GPIO 21 (Pin 40) als Input aditioneel als Pullup-Weerstand aktiveren
 GPIO.setup(DigitalPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+def CallExistingTextFile():
+    f = file(fn, "r+")
+    f = open(fn)
+    inhoud = f.readline()
+    a,b,c = inhoud.split()
+    Counter = int(c)
+    return (Counter, f)
+
+def ReadTextFile():
+    f = open(fn, "w")
+    f.write( 'meterstand = ' + repr(Counter))
+    f.close()
 
 def Interrupt(channel):
     print('Callback function called!')
@@ -39,28 +42,25 @@ def Interrupt(channel):
        print('quitting event handler because this was probably a false positive')
        return
     #Teller elke interrupt uitlezen en met 0.5 liter verhogen (deler watermeter op 10 zetten)
-    file(fn, "r+")
-    f = open(fn)
-    inhoud = f.readline()
-    a,b,c = inhoud.split()
-    Counter = int(c)
+    Counter, f = CallExistingTextFile()
     Counter = Counter + 1
     f.close()
     #Schrijf meterstand naar bestand
-    f = open( fn, 'w')
-    f.write( 'meterstand = ' + repr(Counter))
-    f.close()
+    ReadTextFile()
 
-def CallExistingTextFile():
-    if os.path.exists(fn):
-    f = file(fn, "r+")
-    f = open(fn)
-    inhoud = f.readline()
-    a,b,c = inhoud.split()
-    Counter = int(c)
-    return (Counter)
 
-def CreatNewTextFile():
-    f = open(fn, "w")
-    f.write( 'meterstand = ' + repr(Counter))
-    f.close()
+#main script that will run
+if os.path.exists(fn):
+    Counter =CallExistingTextFile()
+else:
+    ReadTextFile()
+
+GPIO.add_event_detect(40, GPIO.RISING, callback = Interrupt, bouncetime = 200)
+
+#Loop that will run script
+try:
+    while True:
+      time.sleep(0.2)        
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  print "\nBye"
